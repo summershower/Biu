1ab5cd0524f79c96c1ade416fd2b7c1e12c86dc4
kujira <ironys@foxmail.com>
2022-07-01 01:31:52 +0800

    feat:补充部分runtime代码

--- packages/runtime/src/render.ts
+++ packages/runtime/src/render.ts
@@ -1,4 +1,5 @@
-import { VNode, Container } from './types'
+import { DataHTMLAttributes, HTMLAttributes } from 'vue';
+import { VNode, Container, Props, CSSStyleName } from './types'
 import { ShapeFlags } from './vnode'
 /** 渲染函数，将传入的虚拟DOM解析、处理并渲染到页面的真实DOM上 */
 export function render(vnode: VNode, container: Container) {
@@ -22,12 +23,13 @@ function mount(vnode: VNode, container: Container) {
 }
 
 function mountElement(vnode: VNode, container: Container) {
-    const { type } = vnode;
+    const { type, props } = vnode;
     const el = document.createElement(<keyof HTMLElementTagNameMap>type);
-    // mountProps(props, el);
+    props && mountProps(props, el);
     mountChildren(vnode, el)
 }
 
+// Text节点的children就是他的文本内容
 function mountTextNode(vnode: VNode, container: Container) {
     const textNode = document.createTextNode(<string>(vnode.children))
     container.appendChild(textNode)
@@ -40,12 +42,58 @@ function mountComponent(vnode: VNode, container: Container) {
 
 }
 
-function mountChildren(vnode: VNode, container:Container) {
+function mountChildren(vnode: VNode, container: Container) {
     const { shapeFlag, children } = vnode;
     if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         mountTextNode(vnode, container)
     } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-
+        (<[]>children).forEach(child => {
+            mount(child, container)
+        })
     }
+}
+/**
+ * 目前只实现基本类型的props的识别：
+ * 1、class只能是字符串类型
+ * 2、style只能是对象类型
+ * 3、事件只能以on开头，且事件名首字母大写
+ */
 
+/**
+ * About Attributes and DOM Properties
+ * Attr：存在于标签上的属性，Prop：存在于DOM对象的属性
+ * 浏览器在生成DOM对象时，如果检测到标准属性，DOM对象中也会包含对应的属性名（即Prop）
+ * 例如document.body.id。
+ * 而非标准属性不会转化成Prop。
+ * 如：在标签设置了custom = '123' ,document.body.custom = undefined
+ * 而setAttribute则可以将自定义属性设置到Prop中，看似是万能的，但：
+ * 1、setAttribue会把所有属性都设置为string类型，
+ * 2、checked、disabled只要出现了，对应的property就会被初始化为true（即使传递了"false"），只有removeAttribute，才会变成false
+ */
+const domPropsReg = /[A-Z]|^(value|checked|selected|muted|disabled)$/; //A-Z为了匹配innerHTML和textContent
+const eventReg = /^(on)[A-Z][a-zA-Z]*/;
+function mountProps(props: Props, el: Container) {
+    for (const key in props) {
+        const value = props[key]
+        switch (key) {
+            case 'class':
+                el.className = props[value];
+                break;
+            case 'style':
+                for (const styleName in value) {
+                    el.style[<CSSStyleName>styleName] = value[styleName]
+                }
+                break;
+            default:
+                if (eventReg.test(key)) {
+                    // 判定为事件
+                    const eventName = key.slice(2).toLowerCase()
+                    el.addEventListener(eventName, value)
+                } else if(domPropsReg.test(key)) {
+                    // 判定为特殊的Attr，必须直接在DOM上设置
+                    // el[<specAttr>key] = value;
+
+                }
+        }
+    }
 }
--- packages/runtime/src/types.ts
+++ packages/runtime/src/types.ts
@@ -1,5 +1,15 @@
+import { HTMLAttributes, StyleHTMLAttributes } from "vue"
+
 export type Type = keyof HTMLElementTagNameMap | Object | Symbol
-export type Props = Object | null
+export type CSSStyleName = keyof Omit<CSSStyleDeclaration, 'length' | 'parentRule'>
+
+
+export type EventName = keyof HTMLElementTagNameMap
+export type Props = {
+    class?: string,
+    style?: Record<CSSStyleName, string>,
+    [x: string]: any
+} | null
 export type Children = string | Number | [] | null
 
 export interface VNode {
